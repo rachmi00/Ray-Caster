dc250cdc54981cf6f02e53f83972a296b727cb6e
rachmi00 <r.agyingi@gmail.com>
2025-02-10 18:49:27 +0100

    texture addition

--- particle.js
+++ particle.js
@@ -1,78 +1,88 @@
 class Particle {
     constructor() {
-        this.fov = 45
-        this.pos = createVector(width / 2, height / 2);
-        this.rays = [];
-        this.heading = 0;
-        for (let a = -fov; a < fov; a += 1) {
-            this.rays.push(new Ray(this.pos, radians(a)));
-        }
+      this.fov = 45;
+      this.pos = createVector(sceneW / 2, sceneH / 2);
+      this.rays = [];
+      this.heading = 0;
+      for (let a = -this.fov; a < this.fov; a += 1) {
+        this.rays.push(new Ray(this.pos, radians(a)));
+      }
     }
-
-updateFOV(fov) {
-    this.fov = fov;
-    this.rays = [];
-    for (let a = -fov; a < fov; a += 1) {
+  
+    updateFOV(fov) {
+      this.fov = fov;
+      this.rays = [];
+      for (let a = -fov; a < fov; a += 1) {
         this.rays.push(new Ray(this.pos, radians(a) + this.heading));
+      }
     }
-}
-   
-    rotate(angle){
-        this.heading +=angle;
-        let index =0;
-        for (let a = -fov; a < fov; a += 1) {
-            this.rays[index].setAngle(radians(a) + this.heading)
-            index++;
+  
+    rotate(angle) {
+      this.heading += angle;
+      let index = 0;
+      for (let a = -this.fov; a < this.fov; a += 1) {
+        this.rays[index].setAngle(radians(a) + this.heading);
+        index++;
+      }
+    }
+  
+    move(amt) {
+      const vel = p5.Vector.fromAngle(this.heading);
+      vel.setMag(amt);
+      const newPos = p5.Vector.add(this.pos, vel);
+  
+      // Collision detection
+      let collision = false;
+      for (let wall of walls) {
+        if (this.intersectsWall(newPos, wall)) {
+          collision = true;
+          break;
         }
+      }
+  
+      if (!collision) {
+        this.pos.add(vel);
+      }
     }
-
-    move(amt){
-        const vel =p5.Vector.fromAngle(this.heading)
-        vel.setMag(amt);
-        this.pos.add(vel)
+  
+    intersectsWall(pos, wall) {
+      // Simple distance-based collision detection
+      const d = dist(pos.x, pos.y, wall.a.x, wall.a.y);
+      return d < 10; // Adjust threshold as needed
     }
-
-    update(x, y) {
-        this.pos.set(x, y);
-    }
-
   
     look(walls) {
-        const scene =[];
-        for (let i=0 ; i< this.rays.length; i++) {
-            const ray =this.rays[i];
-            let closest = null;
-            let record = Infinity;
-            for (let wall of walls) {
-                const pt = ray.cast(wall);
-
-                if (pt) {
-                    let d = p5.Vector.dist(this.pos, pt);
-                    const a = ray.dir.heading() - this.heading;
-                     d *= cos(a);
-                    if (d < record) {
-                        record = d;
-                        closest = pt;
-                    }
-                }
-
+      const scene = [];
+      for (let i = 0; i < this.rays.length; i++) {
+        const ray = this.rays[i];
+        let closest = null;
+        let record = Infinity;
+        for (let wall of walls) {
+          const pt = ray.cast(wall);
+          if (pt) {
+            let d = p5.Vector.dist(this.pos, pt);
+            const a = ray.dir.heading() - this.heading;
+            d *= cos(a);
+            if (d < record) {
+              record = d;
+              closest = pt;
             }
-            if (closest) {
-                stroke(255,100)
-                line(this.pos.x, this.pos.y, closest.x, closest.y)
-                
-            }
-            scene[i] = record;
+          }
         }
-        return scene;
+        if (closest) {
+          stroke(255, 100);
+          line(this.pos.x, this.pos.y, closest.x, closest.y);
+        }
+        scene[i] = record;
+      }
+      return scene;
     }
-
-
+  
     show() {
-        fill(255);
-        ellipse(this.pos.x, this.pos.y, 16);
-        for (let ray of this.rays) {
-            ray.show();
-        }
+      fill(255);
+      ellipse(this.pos.x, this.pos.y, 16);
+      for (let ray of this.rays) {
+        ray.show();
+      }
     }
-}
+  }
--- sketch.js
+++ sketch.js
@@ -1,96 +1,86 @@
 let walls = [];
-let ray;
 let particle;
-let xoff = 0;
-let yoff = 10000;
 let fov;
 let sliderFOV;
+let textureImg;
 
 const sceneW = 400;
 const sceneH = 300;
 
+function preload() {
+  textureImg = loadImage('texture.jpg'); // Load a texture for the walls
+}
+
 function setup() {
-    createCanvas(800, 300);
+  createCanvas(800, 300);
 
- 
-    for (let i = 0; i < 5; i++) {
-        let x1 = random(sceneW);
-        let x2 = random(sceneW);
-        let y1 = random(sceneH);
-        let y2 = random(sceneH);
-        walls[i] = new Boundary(x1, y1, x2, y2);
-    }
+  // Randomly generate walls
+  for (let i = 0; i < 5; i++) {
+    let x1 = random(sceneW);
+    let x2 = random(sceneW);
+    let y1 = random(sceneH);
+    let y2 = random(sceneH);
+    walls[i] = new Boundary(x1, y1, x2, y2);
+  }
 
-     //putting boundaries along the edges
-     walls.push(new Boundary(0, 0, sceneW, 0));
-     walls.push(new Boundary(sceneW, 0, sceneW, sceneH));
-     walls.push(new Boundary(sceneW, sceneH, 0, sceneH));
-     walls.push(new Boundary(0, sceneH, 0, 0));
- 
- 
-   
-    particle = new Particle();
-    sliderFOV = createSlider(0, 360, 45);
-    sliderFOV.input(changeFOV);
+  // Add boundaries along the edges
+  walls.push(new Boundary(0, 0, sceneW, 0));
+  walls.push(new Boundary(sceneW, 0, sceneW, sceneH));
+  walls.push(new Boundary(sceneW, sceneH, 0, sceneH));
+  walls.push(new Boundary(0, sceneH, 0, 0));
+
+  particle = new Particle();
+  sliderFOV = createSlider(0, 360, 45);
+  sliderFOV.input(changeFOV);
 }
 
 function changeFOV() {
-    fov = sliderFOV.value();
-    particle.updateFOV(fov);
+  fov = sliderFOV.value();
+  particle.updateFOV(fov);
 }
 
-
 function draw() {
+  // Handle player movement
+  if (keyIsDown(LEFT_ARROW)) {
+    particle.rotate(-0.1);
+  } else if (keyIsDown(RIGHT_ARROW)) {
+    particle.rotate(0.1);
+  } else if (keyIsDown(UP_ARROW)) {
+    particle.move(1);
+  } else if (keyIsDown(DOWN_ARROW)) {
+    particle.move(-1);
+  }
 
-    if (keyIsDown(LEFT_ARROW)) {
-        particle.rotate(-0.1);
-    } else if (keyIsDown(RIGHT_ARROW)) {
-        particle.rotate(0.1);
-    }else if (keyIsDown(UP_ARROW)){
-        particle.move(1);
-    }else if (keyIsDown(DOWN_ARROW)){
-        particle.move(-1);
-    }
+  background(0);
 
+  // Draw walls in 2D view
+  for (let wall of walls) {
+    wall.show();
+  }
 
-    background(0);
-    for (let wall of walls) {
-        wall.show();
-    }
+  // Draw the particle (player)
+  particle.show();
 
-    //particle.update(noise(xoff) *sceneW, noise(yoff) * sceneH)
-    //particle.update(mouseX, mouseY);
-    particle.show();
+  // Render the first-person view
+  const scene = particle.look(walls);
+  const w = sceneW / scene.length;
+  push();
+  translate(sceneW, 0);
+  for (let i = 0; i < scene.length; i++) {
+    noStroke();
+    const sq = scene[i] * scene[i];
+    const wsq = sceneW * sceneW;
+    const b = map(sq, 0, wsq, 255, 0); // Dynamic lighting based on distance
+    const h = map(scene[i], 0, sceneW, sceneH, 0);
 
-     //xoff += 0.01
-    //yoff += 0.01
+    // Texture mapping
+    const texX = i % textureImg.width;
+    const texY = map(h, 0, sceneH, 0, textureImg.height);
+    const texColor = textureImg.get(texX, texY);
+    fill(texColor);
 
-
-    //return array scene
-    const scene = particle.look(walls);
-    const w = sceneW / scene.length;
-    push();
-    translate(sceneW, 0)
-    for (let i = 0; i < scene.length; i++) {
-        noStroke();
-        const sq = scene[i] * scene[i];
-        const wsq = sceneW * sceneW;
-        //let c = color(255, 128, 128);
-        const b = map(sq, 0, wsq, 255, 0)
-        const h = map(scene[i], 0, sceneW, sceneH, 0)
-        fill(b);
-        rectMode(CENTER)
-        rect(i * w + w / 2, sceneH / 2, w +1, h);
-    }
-    pop();
-
-
-    //cast a ray against a wall 
-    // let pt = ray.cast(wall);
-    //if the point exists
-    // console.log(pt)
-    // if(pt) {
-    //     fill(255);
-    //     ellipse(pt.x, pt.y, 8, 8);
-    // }
+    rectMode(CENTER);
+    rect(i * w + w / 2, sceneH / 2, w + 1, h);
+  }
+  pop();
 }
